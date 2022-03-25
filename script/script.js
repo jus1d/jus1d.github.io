0ad910f7b09a44f75af6d1c4a017d2093b84c3aa
jus1d <artfa63@yandex.ru>
2022-03-25 23:44:59 +0400

    back

--- script/script.js
+++
@@ -1,8 +0,0 @@
-function turn_graph(id){
-    var image = document.getElementById(id);
-
-    if (image.style.display == "block")
-        image.style.display = "none";
-    else
-        image.style.display = "block";
-}
